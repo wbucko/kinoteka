00442063b520437e83a66f396ac4e4557d1b7f20
wbucko <bucko.wojciech@gmail.com>
2016-01-25 20:27:55 +0100

    Created table for users, added routes and model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160124162442) do
+ActiveRecord::Schema.define(version: 20160125191906) do
 
   create_table "movies", force: :cascade do |t|
     t.string   "title"
@@ -23,6 +23,14 @@ ActiveRecord::Schema.define(version: 20160124162442) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.text     "keywords"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name"
+    t.string   "email"
+    t.string   "password_digest"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
 end
