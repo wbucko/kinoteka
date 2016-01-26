2a90ebdc34e1e3ac0eca11fc609791d62480e1e5
wbucko <bucko.wojciech@gmail.com>
2016-01-26 23:21:12 +0100

    Created sessions controller, routes and view for login

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,24 @@
+class SessionsController < ApplicationController
+
+	def new
+	end
+
+	def create
+		user = User.find_by(email: params[:email])
+		if user && user.authenticate(params[:password])
+			session[:user_id] = user.id
+			flash[:success] = 'Zostałeś zalogowany.'
+			redirect_to root_path
+		else
+			flash.now[:danger] = 'Twój email lub hasło jest niepoprawne.'
+			render :new
+		end
+	end
+
+	def destroy
+		session[:user_id] = nil
+		flash[:success] = 'Zostałes wylogowany.'
+		redirect_to root_path
+	end
+
+end
