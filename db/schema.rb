d80cf52f988f354906a4a99fba8f40d8e02d3177
wbucko <bucko.wojciech@gmail.com>
2016-01-27 18:19:49 +0100

    Added admin column to users and methods current_user and logged_in?

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160125191906) do
+ActiveRecord::Schema.define(version: 20160127170701) do
 
   create_table "movies", force: :cascade do |t|
     t.string   "title"
@@ -31,6 +31,7 @@ ActiveRecord::Schema.define(version: 20160125191906) do
     t.string   "password_digest"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.boolean  "admin",           default: false
   end
 
 end
