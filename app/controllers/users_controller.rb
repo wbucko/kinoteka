1fb4d4e0c75859278531c588a4052a36649016c8
wbucko <bucko.wojciech@gmail.com>
2016-04-04 10:05:50 +0200

    Added password confirmation to the page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -40,7 +40,7 @@ class UsersController < ApplicationController
 
 	private
 	def user_params
-		params.require(:user).permit(:name, :email, :password)
+		params.require(:user).permit(:name, :email, :password, :password_confirmation)
 	end
 
   def find_user
--- app/models/user.rb
+++ app/models/user.rb
@@ -15,4 +15,5 @@ class User < ActiveRecord::Base
   validates :email, presence: true, length: { maximum: 105 },
                                 uniqueness: { case_sensitive: false },
                                     format: { with: VALID_EMAIL_REGEX }
+
 end
