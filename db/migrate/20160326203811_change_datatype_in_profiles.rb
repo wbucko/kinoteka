68685d5586e22d5f3c68860bbb066c8348bf39a3
wbucko <bucko.wojciech@gmail.com>
2016-04-04 10:40:24 +0200

    Fixed migration for postgres

--- db/migrate/20160326203811_change_datatype_in_profiles.rb
+++ db/migrate/20160326203811_change_datatype_in_profiles.rb
@@ -1,6 +1,7 @@
 class ChangeDatatypeInProfiles < ActiveRecord::Migration
   def change
-  	change_column :profiles, :user_id, :integer
+  	# change_column :profiles, :user_id, :integer
+  	change_column :profiles, :user_id, 'integer USING CAST(user_id AS integer)'
   end
 end
 
